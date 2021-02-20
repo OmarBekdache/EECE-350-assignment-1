639ae3b6d0deaf349357ec69677f296ea25c694f
OmarBekdache <79358933+OmarBekdache@users.noreply.github.com>
2021-02-20 12:18:11 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+import datetime
+
+clientSocket = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
+echoServerIP = input("Enter the IP address of the echo server")
+echoServerPort = int(input("Enter the port number of the echo server"))
+echoMSG = ['echo message 1', 'echo message 2', 'echo message 3', 'echo message 4', 'echo message 5']
+averageRTT = 0
+
+for i in range (0,5):
+    t1 = datetime.datetime.now()
+    clientSocket.sendto(echoMSG[i].encode('utf-8'), (echoServerIP, echoServerPort))
+    data, address = clientSocket.recvfrom(1024)
+    t2 = datetime.datetime.now()
+    averageRTT += (t2 - t1).microseconds
+    print ("Round trip time ", i+1, " ", (t2-t1).microseconds, "microseconds")
+
+averageRTT = averageRTT/5
+print('Average RTT: ', averageRTT, " microseconds")
+clientSocket.close()
